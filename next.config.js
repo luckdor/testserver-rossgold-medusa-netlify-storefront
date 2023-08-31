20e0b0db41a63bfdf6ed28b9e243d2b6dfc1bc16
luckdor <92556306+luckdor@users.noreply.github.com>
2023-08-31 11:40:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    // domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "mwb-dhy00032.koyeb.app/uploads/images/"],
-    domains: ["http://mwb-dhy00032.koyeb.app","http://mwb-dhy00032.koyeb.app/uploads/images"],
-  },
+    // domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "mwb-dhy00032.koyeb.app/uploads/images/"]
+    domains: [
+        // ...
+        "https://mwb-dhy00032.koyeb.app",
+      ],
 })
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
