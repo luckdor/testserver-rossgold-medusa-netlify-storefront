bd0e884860672191b8f32165f94c4c2fff713303
luckdor <92556306+luckdor@users.noreply.github.com>
2023-09-23 21:01:21 +0800

    Update index.tsx

--- src/modules/layout/components/footer-nav/index.tsx
+++ src/modules/layout/components/footer-nav/index.tsx
@@ -1,87 +1,3 @@
-// import clsx from "clsx"
-// import { useCollections } from "medusa-react"
-// import Link from "next/link"
-// import CountrySelect from "../country-select"
-
-// const FooterNav = () => {
-//   const { collections } = useCollections()
-
-//   return (
-//     <div className="content-container flex flex-col gap-y-8 pt-16 pb-8">
-//       <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
-//         <div>
-//           <Link href="/">
-//             <a className="text-xl-semi uppercase">Acme</a>
-//           </Link>
-//         </div>
-//         <div className="text-small-regular grid grid-cols-2 gap-x-16">
-//           <div className="flex flex-col gap-y-2">
-//             <span className="text-base-semi">Collections</span>
-//             <ul
-//               className={clsx("grid grid-cols-1 gap-y-2", {
-//                 "grid-cols-2": (collections?.length || 0) > 4,
-//               })}
-//             >
-//               {collections?.map((c) => (
-//                 <li key={c.id}>
-//                   <Link href={`/collections/${c.id}`}>
-//                     <a>{c.title}</a>
-//                   </Link>
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//           <div className="flex flex-col gap-y-2">
-//             <span className="text-base-semi">Medusa</span>
-//             <ul className="grid grid-cols-1 gap-y-2">
-//               <li>
-//                 <a
-//                   href="https://photos.onedrive.com/share/42DCABA6199E2232!148?cid=42DCABA6199E2232&resId=42DCABA6199E2232!148&authkey=!AOVi5pl8GZ5SfjA&ithint=photo&e=LWs1AP"
-//                   target="_blank"
-//                   rel="noreferrer"
-//                 >
-//                   Contact us via wechat scanning
-//                 </a>
-//               </li>
-//               <li>
-//                 <a
-//                   href="https://www.facebook.com/profile.php?id=61550993903253&mibextid=ZbWKwL"
-//                   target="_blank"
-//                   rel="noreferrer"
-//                 >
-//                   Contact us via facebook page
-//                 </a>
-//               </li>
-//               <li>
-//                 <a
-//                   href="https://wica.wica.ontheweb.nu"
-//                   target="_blank"
-//                   rel="noreferrer"
-//                 >
-//                   Homepage
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
-//         <span className="text-xsmall-regular text-gray-500">
-//           © Copyright 2023 WICA
-//         </span>
-//         <div className="min-w-[316px] flex xsmall:justify-end">
-//           <CountrySelect />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default FooterNav
-
-
-
-
 import clsx from "clsx"
 import { useCollections } from "medusa-react"
 import Link from "next/link"
@@ -124,7 +40,7 @@ const FooterNav = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  Contact us via wechat scanning
+                  Wechat scanning
                 </a>
               </li>
               <li>
@@ -133,7 +49,7 @@ const FooterNav = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  Contact us via facebook page
+                  Facebook page
                 </a>
               </li>
               <li>
@@ -142,7 +58,7 @@ const FooterNav = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  Contact via tiktok scanning
+                  Tiktok scanning
                 </a>
               </li>
             </ul>
