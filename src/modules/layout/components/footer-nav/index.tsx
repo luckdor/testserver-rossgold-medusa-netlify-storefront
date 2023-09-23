56cfaaa22926a3b40096b90a80cbe6e8588b0148
luckdor <92556306+luckdor@users.noreply.github.com>
2023-09-23 15:32:32 +0800

    Update index.tsx

--- src/modules/layout/components/footer-nav/index.tsx
+++ src/modules/layout/components/footer-nav/index.tsx
@@ -120,7 +120,7 @@ const FooterNav = () => {
             <ul className="grid grid-cols-1 gap-y-2">
               <li>
                 <a
-                  href="https://photos.onedrive.com/share/42DCABA6199E2232!148?cid=42DCABA6199E2232&resId=42DCABA6199E2232!148&authkey=!AOVi5pl8GZ5SfjA&ithint"
+                  href="https://photos.onedrive.com/share/42DCABA6199E2232!148?cid=42DCABA6199E2232&resId=42DCABA6199E2232!148&authkey=!AOVi5pl8GZ5SfjA&ithint=photo&e=LWs1AP"
                   target="_blank"
                   rel="noreferrer"
                 >
