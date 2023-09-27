b2de15b39c439e7f3e8a74ce1b24dc244da25d25
luckdor <92556306+luckdor@users.noreply.github.com>
2023-09-27 20:51:12 +0800

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,11 +6,10 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here
+          Your eyes are more mysterious and powerful than you think
         </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black"> 
+          they are full of energy! Come, make the window of your soul more beautiful and stronger!
         </p>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
